1b54dfa3540003c1b558734e14ba9d9b8ce92bd1
Jonas Devlieghere <jonas@devlieghere.com>
2016-08-01 17:47:10 +0200

    Trying to fix travis build on linux

--- .travis/dependencies.sh
+++ .travis/dependencies.sh
@@ -3,11 +3,26 @@
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
   # Install dependencies for macOS
   brew update
-  brew instal libxml2
-  brew instal llvm
+  brew install libxml2
+  brew install llvm
 else
   # Install dependencies for Linux
   sudo apt-get -qq update
   sudo apt-get install -y libxml2-dev
-  sudo apt-get install -y llvm-3.8-dev
+
+  # Build LLVM from source
+  sudo mkdir -p /opt/llvm
+
+  git clone http://llvm.org/git/llvm.git llvm
+  cd llvm
+  git checkout release_38
+  cd ..
+
+  mkdir build
+  cd build
+  cmake ../llvm -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/opt/llvm
+  make -j
+  sudo make install
+  cd ..
 fi
+
