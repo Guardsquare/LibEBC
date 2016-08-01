56675c32a74bc46a74ad00f777054b085315ccb2
Jonas Devlieghere <jonas@devlieghere.com>
2016-08-01 14:12:43 +0200

    Have travis use LLVM 3.8

--- .travis/dependencies.sh
+++ .travis/dependencies.sh
@@ -9,5 +9,5 @@ else
   # Install dependencies for Linux
   sudo apt-get -qq update
   sudo apt-get install -y libxml2-dev
-  sudo apt-get install -y llvm-dev
+  sudo apt-get install -y llvm-3.8-dev
 fi
