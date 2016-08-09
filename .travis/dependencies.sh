f457a0ffad89184f7a84c0b5ffb50503c1599919
Jonas Devlieghere <jonas@devlieghere.com>
2016-08-09 19:28:17 +0200

    Have Travis use CMake 3.6

--- .travis/dependencies.sh
+++ .travis/dependencies.sh
@@ -10,6 +10,12 @@ else
   sudo apt-get -qq update
   sudo apt-get install -y libxml2-dev
 
+  # Use CMake 3.6.1
+  wget https://cmake.org/files/v3.6/cmake-3.6.1-Linux-x86_64.tar.gz
+  tar xf cmake-3.6.1-Linux-x86_64.tar.gz
+  export PATH=$(pwd)/cmake-3.6.1-Linux-x86_64/bin:$PATH
+
+  # Get LLVM 3.8.1
   wget http://llvm.org/releases/3.8.1/clang+llvm-3.8.1-x86_64-linux-gnu-ubuntu-14.04.tar.xz
   tar -xJf clang+llvm-3.8.1-x86_64-linux-gnu-ubuntu-14.04.tar.xz
   sudo mkdir -p $LLVM_HINT
