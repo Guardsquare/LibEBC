b50f29d98b4e222d3bf7079dfb0b94400ccad64f
Jonas Devlieghere <jonas@devlieghere.com>
2017-02-11 15:08:45 +0100

    Better error messages for retrieving bitcode.
    The retriever implementation now consistently passes around errors. This
    means that all errors are properly handled and that no information is
    accidentally lost throughout the chain.

--- lib/include/ebc/BitcodeRetriever.h
+++ lib/include/ebc/BitcodeRetriever.h
@@ -4,16 +4,6 @@
 #include <string>
 #include <vector>
 
-namespace llvm {
-namespace object {
-class Archive;
-class Binary;
-class MachOObjectFile;
-class ObjectFile;
-class SectionRef;
-}
-}
-
 namespace ebc {
 class BitcodeContainer;
 
@@ -24,6 +14,7 @@ class BitcodeRetriever {
   ///
   /// @param objectPath The file from which to retrieve bitcode.
   BitcodeRetriever(std::string objectPath);
+  ~BitcodeRetriever();
 
   /// Set the architecture for which to retrieve bitcode. This is relevant to
   /// 'fat' object files containing multiple architectures. If no architecture
@@ -40,61 +31,7 @@ class BitcodeRetriever {
   std::vector<std::unique_ptr<BitcodeContainer>> GetBitcodeContainers();
 
  private:
-  /// Helper method for determining whether the given architecture should be
-  /// handled.
-  ///
-  /// @param arch The architecture.
-  ///
-  /// @return True if the architecture matches the set architure or when no
-  /// architecture is set. False otherwise.
-  bool processArch(std::string arch) const;
-
-  /// Obtains all bitcode from an object. The method basically determines the
-  /// kind of object and dispatches the actual work to the specialized method.
-  ///
-  /// @param binary The binary object.
-  ///
-  /// @return A list of bitcode containers.
-  std::vector<std::unique_ptr<BitcodeContainer>> GetBitcodeContainers(const llvm::object::Binary& binary) const;
-
-  /// Obtains all bitcode from an object archive.
-  ///
-  /// @param archive The object archive.
-  ///
-  /// @return A list of bitcode containers.
-  std::vector<std::unique_ptr<BitcodeContainer>> GetBitcodeContainersFromArchive(
-      const llvm::object::Archive& archive) const;
-
-  /// Reads bitcode from a Mach O object file.
-  ///
-  /// @param objectFile The Mach O object file.
-  ///
-  /// @return The bitcode container.
-  std::unique_ptr<BitcodeContainer> GetBitcodeContainerFromMachO(const llvm::object::MachOObjectFile* objectFile) const;
-
-  /// Reads bitcode from a plain object file.
-  ///
-  /// @param objectFile The Mach O object file.
-  ///
-  /// @return The bitcode container.
-  std::unique_ptr<BitcodeContainer> GetBitcodeContainerFromObject(const llvm::object::ObjectFile* objectFile) const;
-
-  /// Obtains data from a section.
-  ///
-  /// @param section The section from which the data should be obtained.
-  ///
-  /// @return A pair with the data and the size of the data.
-  static std::pair<const char*, std::uint32_t> GetSectionData(const llvm::object::SectionRef& section);
-
-  /// Obtains compiler commands from a section. It automatically parses the
-  /// data into a vector.
-  ///
-  /// @param section The sectio from which to read the compiler commands.
-  ///
-  /// @return A vector of compiler commands.
-  static std::vector<std::string> GetCommands(const llvm::object::SectionRef& section);
-
-  std::string _objectPath;
-  std::string _arch;
+  class Impl;
+  std::unique_ptr<Impl> _impl;
 };
 }
--- lib/src/BitcodeRetriever.cpp
+++ lib/src/BitcodeRetriever.cpp
@@ -8,6 +8,8 @@
 #include "llvm/Object/ELFObjectFile.h"
 #include "llvm/Object/MachO.h"
 #include "llvm/Object/MachOUniversal.h"
+#include "llvm/Support/Error.h"
+#include "llvm/Support/ErrorHandling.h"
 
 #include <algorithm>
 #include <iostream>
@@ -25,213 +27,319 @@ static std::string StripFileName(std::string fileName) {
   return pos == std::string::npos ? fileName : fileName.substr(pos + 1);
 }
 
-BitcodeRetriever::BitcodeRetriever(std::string objectPath) : _objectPath(std::move(objectPath)) {}
+/// Internal EBC error that extends LLVM's ErrorInfo class. We use this to have
+/// the implementation return llvm::Expected<> objects.
+class InternalEbcError : public llvm::ErrorInfo<InternalEbcError> {
+ public:
+  static char ID;
 
-void BitcodeRetriever::SetArch(std::string arch) {
-  _arch = arch;
-}
+  InternalEbcError(std::string msg) : _msg(std::move(msg)) {}
 
-std::vector<std::unique_ptr<BitcodeContainer>> BitcodeRetriever::GetBitcodeContainers() {
-  auto binaryOrErr = createBinary(_objectPath);
-
-  if (binaryOrErr) {
-    auto &binary = *binaryOrErr.get().getBinary();
-    return GetBitcodeContainers(binary);
-  } else {
-    llvm::consumeError(binaryOrErr.takeError());
-    throw EbcError("Could not create binary from " + _objectPath);
+  const std::string &getMessage() const {
+    return _msg;
   }
 
-  return {};
+  std::error_code convertToErrorCode() const override {
+    llvm_unreachable("Not implemented");
+  }
+
+  void log(llvm::raw_ostream &OS) const override {
+    OS << _msg;
+  }
+
+ private:
+  std::string _msg;
+};
+
+char InternalEbcError::ID = 0;
+
+/// Convert an LLVM error object to an EbcError. This works both for LLVM's
+/// internal errors as well as for our custom InternalEbcError.
+static EbcError ToEbcError(llvm::Error &&e) {
+  std::string msg;
+  handleAllErrors(std::move(e), [&msg](llvm::ErrorInfoBase &eib) { msg = eib.message(); });
+  return EbcError(msg);
 }
 
-std::vector<std::unique_ptr<BitcodeContainer>> BitcodeRetriever::GetBitcodeContainers(
-    const llvm::object::Binary &binary) const {
-  auto bitcodeContainers = std::vector<std::unique_ptr<BitcodeContainer>>();
+class BitcodeRetriever::Impl {
+ public:
+  typedef std::vector<std::unique_ptr<BitcodeContainer>> BitcodeContainers;
 
-  if (const auto *universalBinary = dyn_cast<MachOUniversalBinary>(&binary)) {
-    // Iterate over all objects (i.e. architectures):
-    for (auto object : universalBinary->objects()) {
-      Expected<std::unique_ptr<MachOObjectFile>> machOObject = object.getAsObjectFile();
-      if (machOObject) {
-        auto container = GetBitcodeContainerFromMachO(machOObject->get());
-        if (container) {
-          bitcodeContainers.push_back(std::move(container));
+  Impl(std::string objectPath) : _objectPath(std::move(objectPath)) {}
+
+  /// Set the architecture for which to retrieve bitcode. This is relevant to
+  /// 'fat' object files containing multiple architectures. If no architecture
+  /// is set, bitcode is retrieved for all present architectures.
+  void SetArch(std::string arch) {
+    _arch = std::move(arch);
+  }
+
+  /// Perform the actual bitcode retrieval. Depending on the type of the object
+  /// file the resulting list contains plain bitcode containers or bitcode
+  /// archives.
+  ///
+  /// @return A list of bitcode containers.
+  BitcodeContainers GetBitcodeContainers() {
+    auto binaryOrErr = createBinary(_objectPath);
+    if (!binaryOrErr) {
+      throw ToEbcError(binaryOrErr.takeError());
+    }
+
+    auto bitcodeContainers = GetBitcodeContainers(*binaryOrErr->getBinary());
+    if (!bitcodeContainers) {
+      throw ToEbcError(bitcodeContainers.takeError());
+    }
+
+    return std::move(*bitcodeContainers);
+  }
+
+ private:
+  /// Helper method for determining whether the given architecture should be
+  /// handled.
+  ///
+  /// @param arch The architecture.
+  ///
+  /// @return True if the architecture matches the set architure or when no
+  /// architecture is set. False otherwise.
+  bool processArch(std::string arch) const {
+    return _arch.empty() ? true : (_arch == arch);
+  }
+
+  /// Obtains all bitcode from an object. The method basically determines the
+  /// kind of object and dispatches the actual work to the specialized method.
+  ///
+  /// @param binary The binary object.
+  ///
+  /// @return A list of bitcode containers.
+  llvm::Expected<BitcodeContainers> GetBitcodeContainers(const llvm::object::Binary &binary) const {
+    auto bitcodeContainers = std::vector<std::unique_ptr<BitcodeContainer>>();
+
+    if (const auto *universalBinary = dyn_cast<MachOUniversalBinary>(&binary)) {
+      // Iterate over all objects (i.e. architectures):
+      for (auto object : universalBinary->objects()) {
+        Expected<std::unique_ptr<MachOObjectFile>> machOObject = object.getAsObjectFile();
+        if (machOObject) {
+          auto container = GetBitcodeContainerFromMachO(machOObject->get());
+          if (!container) {
+            return container.takeError();
+          }
+          bitcodeContainers.push_back(std::move(container.get()));
+          continue;
+        } else {
+          llvm::consumeError(machOObject.takeError());
         }
-        continue;
-      } else {
-        llvm::consumeError(machOObject.takeError());
+
+        Expected<std::unique_ptr<Archive>> archive = object.getAsArchive();
+        if (archive) {
+          auto containers = GetBitcodeContainersFromArchive(*archive->get());
+          if (!containers) {
+            return containers.takeError();
+          }
+
+          // We have to move all valid containers so we can move on to the next
+          // architecture.
+          bitcodeContainers.reserve(bitcodeContainers.size() + containers->size());
+          std::copy_if(std::make_move_iterator(containers->begin()), std::make_move_iterator(containers->end()),
+                       std::back_inserter(bitcodeContainers),
+                       [](const auto &uniquePtr) { return uniquePtr != nullptr; });
+          continue;
+        } else {
+          llvm::consumeError(archive.takeError());
+        }
       }
-
-      Expected<std::unique_ptr<Archive>> archive = object.getAsArchive();
-      if (archive) {
-        auto containers = GetBitcodeContainersFromArchive(*archive->get());
-        // We have to move all containers so we can continue with the next architecture.
-        bitcodeContainers.reserve(bitcodeContainers.size() + containers.size());
-        std::copy_if(std::make_move_iterator(containers.begin()), std::make_move_iterator(containers.end()),
-                     std::back_inserter(bitcodeContainers), [](const auto &uniquePtr) { return uniquePtr != nullptr; });
-        continue;
-      } else {
-        llvm::consumeError(archive.takeError());
+    } else if (const auto machOObjectFile = dyn_cast<MachOObjectFile>(&binary)) {
+      auto container = GetBitcodeContainerFromMachO(machOObjectFile);
+      if (!container) {
+        return container.takeError();
       }
-
-      throw EbcError("Unrecognized MachO universal binary");
+      bitcodeContainers.push_back(std::move(*container));
+    } else if (const auto object = dyn_cast<ObjectFile>(&binary)) {
+      auto container = GetBitcodeContainerFromObject(object);
+      if (!container) {
+        return container.takeError();
+      }
+      bitcodeContainers.push_back(std::move(*container));
+    } else if (const auto archive = dyn_cast<Archive>(&binary)) {
+      // We can return early to prevent moving all containers in the vector.
+      return GetBitcodeContainersFromArchive(*archive);
+    } else {
+      return llvm::make_error<InternalEbcError>("Unsupported binary");
     }
-  } else if (const auto machOObjectFile = dyn_cast<MachOObjectFile>(&binary)) {
-    auto container = GetBitcodeContainerFromMachO(machOObjectFile);
-    if (container) {
-      bitcodeContainers.push_back(std::move(container));
+
+    return std::move(bitcodeContainers);
+  }
+
+  /// Obtains all bitcode from an object archive.
+  ///
+  /// @param archive The object archive.
+  ///
+  /// @return A list of bitcode containers.
+  llvm::Expected<BitcodeContainers> GetBitcodeContainersFromArchive(const llvm::object::Archive &archive) const {
+    Error err = Error::success();
+    auto bitcodeContainers = std::vector<std::unique_ptr<BitcodeContainer>>();
+    for (const auto &child : archive.children(err)) {
+      auto childOrErr = child.getAsBinary();
+      if (!childOrErr) {
+        return childOrErr.takeError();
+      }
+      auto containers = GetBitcodeContainers(*(*childOrErr));
+      bitcodeContainers.reserve(bitcodeContainers.size() + containers->size());
+      std::move(containers->begin(), containers->end(), std::back_inserter(bitcodeContainers));
     }
-  } else if (const auto object = dyn_cast<ObjectFile>(&binary)) {
-    auto container = GetBitcodeContainerFromObject(object);
-    if (container) {
-      bitcodeContainers.push_back(std::move(container));
+
+    if (err) {
+      return std::move(err);
     }
-  } else if (const auto archive = dyn_cast<Archive>(&binary)) {
-    // We can return early to prevent moving all containers in the vector.
-    return GetBitcodeContainersFromArchive(*archive);
-  } else {
-    throw EbcError("Unsupported binary");
+
+    return std::move(bitcodeContainers);
   }
 
-  return bitcodeContainers;
-}
-
-std::vector<std::unique_ptr<BitcodeContainer>> BitcodeRetriever::GetBitcodeContainersFromArchive(
-    const Archive &archive) const {
-  Error err;
-  auto children = archive.children(err);
-
-  if (err) {
-    throw EbcError("Couldn't get children from archive " + archive.getFileName().str());
-  }
-
-  auto bitcodeContainers = std::vector<std::unique_ptr<BitcodeContainer>>();
-  for (const auto &child : children) {
-    auto childOrErr = child.getAsBinary();
-    if (childOrErr) {
-      auto containers = GetBitcodeContainers(*(childOrErr.get()));
-      bitcodeContainers.reserve(bitcodeContainers.size() + containers.size());
-      std::move(std::begin(containers), std::end(containers), std::back_inserter(bitcodeContainers));
+  /// Reads bitcode from a Mach O object file.
+  ///
+  /// @param objectFile The Mach O object file.
+  ///
+  /// @return The bitcode container.
+  llvm::Expected<std::unique_ptr<BitcodeContainer>> GetBitcodeContainerFromMachO(
+      const llvm::object::MachOObjectFile *objectFile) const {
+    // For MachO return the correct arch tripple.
+    const std::string arch = objectFile->getArchTriple(nullptr).getArchName();
+    if (!processArch(arch)) {
+      return nullptr;
     }
-  }
-  return bitcodeContainers;
-}
 
-std::unique_ptr<BitcodeContainer> BitcodeRetriever::GetBitcodeContainerFromObject(
-    const llvm::object::ObjectFile *objectFile) const {
-  const auto arch = llvm::Triple::getArchTypeName(static_cast<Triple::ArchType>(objectFile->getArch()));
-  if (!processArch(arch)) {
-    return {};
-  }
+    BitcodeContainer *bitcodeContainer = nullptr;
 
-  BitcodeContainer *bitcodeContainer = nullptr;
+    const std::string name = objectFile->getFileFormatName().str();
+    std::vector<std::string> commands;
+    for (const SectionRef &section : objectFile->sections()) {
+      // Get section name
+      DataRefImpl dataRef = section.getRawDataRefImpl();
+      StringRef segName = objectFile->getSectionFinalSegmentName(dataRef);
 
-  std::vector<std::string> commands;
-  for (const SectionRef &section : objectFile->sections()) {
-    StringRef sectName;
-    section.getName(sectName);
-
-    if (sectName == ".llvmbc") {
-      auto data = GetSectionData(section);
-      bitcodeContainer = new BitcodeContainer(data.first, data.second);
-    } else if (sectName == ".llvmcmd") {
-      commands = GetCommands(section);
+      if (segName == "__LLVM") {
+        StringRef sectName;
+        section.getName(sectName);
+        if (sectName == "__bundle") {
+          // Embedded bitcode in universal binary.
+          auto data = GetSectionData(section);
+          bitcodeContainer = new BitcodeArchive(data.first, data.second);
+        } else if (sectName == "__bitcode") {
+          // Embedded bitcode in single MachO object.
+          auto data = GetSectionData(section);
+          bitcodeContainer = new BitcodeContainer(data.first, data.second);
+        } else if (sectName == "__cmd" || sectName == "__cmdline") {
+          commands = GetCommands(section);
+        }
+      }
     }
+
+    if (bitcodeContainer == nullptr) {
+      return llvm::make_error<InternalEbcError>("No bitcode section in " + objectFile->getFileName().str());
+    }
+
+    // Set commands
+    bitcodeContainer->SetCommands(commands);
+
+    // Set binary metadata
+    bitcodeContainer->GetBinaryMetadata().SetFileName(StripFileName(objectFile->getFileName()));
+    bitcodeContainer->GetBinaryMetadata().SetFileFormatName(objectFile->getFileFormatName());
+    bitcodeContainer->GetBinaryMetadata().SetArch(arch);
+    bitcodeContainer->GetBinaryMetadata().SetUuid(objectFile->getUuid().data());
+
+    return std::unique_ptr<BitcodeContainer>(bitcodeContainer);
   }
 
-  if (bitcodeContainer == nullptr) {
-    throw NoBitcodeError("No bitcode section in " + objectFile->getFileName().str());
-  }
+  /// Reads bitcode from a plain object file.
+  ///
+  /// @param objectFile The Mach O object file.
+  ///
+  /// @return The bitcode container.
+  llvm::Expected<std::unique_ptr<BitcodeContainer>> GetBitcodeContainerFromObject(
+      const llvm::object::ObjectFile *objectFile) const {
+    const auto arch = llvm::Triple::getArchTypeName(static_cast<Triple::ArchType>(objectFile->getArch()));
+    if (!processArch(arch)) {
+      return nullptr;
+    }
 
-  // Set commands
-  bitcodeContainer->SetCommands(commands);
+    BitcodeContainer *bitcodeContainer = nullptr;
 
-  // Set binary metadata
-  bitcodeContainer->GetBinaryMetadata().SetFileName(StripFileName(objectFile->getFileName()));
-  bitcodeContainer->GetBinaryMetadata().SetFileFormatName(objectFile->getFileFormatName());
-  bitcodeContainer->GetBinaryMetadata().SetArch(arch);
-
-  return std::unique_ptr<BitcodeContainer>(bitcodeContainer);
-}
-
-std::unique_ptr<BitcodeContainer> BitcodeRetriever::GetBitcodeContainerFromMachO(
-    const llvm::object::MachOObjectFile *objectFile) const {
-  // For MachO return the correct arch tripple.
-  const std::string arch = objectFile->getArchTriple(nullptr).getArchName();
-  if (!processArch(arch)) {
-    return {};
-  }
-
-  BitcodeContainer *bitcodeContainer = nullptr;
-
-  const std::string name = objectFile->getFileFormatName().str();
-  std::vector<std::string> commands;
-  for (const SectionRef &section : objectFile->sections()) {
-    // Get section name
-    DataRefImpl dataRef = section.getRawDataRefImpl();
-    StringRef segName = objectFile->getSectionFinalSegmentName(dataRef);
-
-    if (segName == "__LLVM") {
+    std::vector<std::string> commands;
+    for (const SectionRef &section : objectFile->sections()) {
       StringRef sectName;
       section.getName(sectName);
-      if (sectName == "__bundle") {
-        // Embedded bitcode in universal binary.
-        auto data = GetSectionData(section);
-        bitcodeContainer = new BitcodeArchive(data.first, data.second);
-      } else if (sectName == "__bitcode") {
-        // Embedded bitcode in single MachO object.
+
+      if (sectName == ".llvmbc") {
         auto data = GetSectionData(section);
         bitcodeContainer = new BitcodeContainer(data.first, data.second);
-      } else if (sectName == "__cmd" || sectName == "__cmdline") {
+      } else if (sectName == ".llvmcmd") {
         commands = GetCommands(section);
       }
     }
+
+    if (bitcodeContainer == nullptr) {
+      return llvm::make_error<InternalEbcError>("No bitcode section in " + objectFile->getFileName().str());
+    }
+
+    // Set commands
+    bitcodeContainer->SetCommands(commands);
+
+    // Set binary metadata
+    bitcodeContainer->GetBinaryMetadata().SetFileName(StripFileName(objectFile->getFileName()));
+    bitcodeContainer->GetBinaryMetadata().SetFileFormatName(objectFile->getFileFormatName());
+    bitcodeContainer->GetBinaryMetadata().SetArch(arch);
+
+    return std::unique_ptr<BitcodeContainer>(bitcodeContainer);
   }
 
-  if (bitcodeContainer == nullptr) {
-    throw NoBitcodeError("No bitcode section in " + objectFile->getFileName().str());
+  /// Obtains data from a section.
+  ///
+  /// @param section The section from which the data should be obtained.
+  ///
+  /// @return A pair with the data and the size of the data.
+  static std::pair<const char *, std::uint32_t> GetSectionData(const llvm::object::SectionRef &section) {
+    StringRef bytesStr;
+    section.getContents(bytesStr);
+    const char *sect = reinterpret_cast<const char *>(bytesStr.data());
+    uint32_t sect_size = bytesStr.size();
+    return std::make_pair(sect, sect_size);
   }
 
-  // Set commands
-  bitcodeContainer->SetCommands(commands);
+  /// Obtains compiler commands from a section. It automatically parses the
+  /// data into a vector.
+  ///
+  /// @param section The sectio from which to read the compiler commands.
+  ///
+  /// @return A vector of compiler commands.
+  static std::vector<std::string> GetCommands(const llvm::object::SectionRef &section) {
+    auto data = GetSectionData(section);
+    const char *p = data.first;
+    const char *end = data.first + data.second;
 
-  // Set binary metadata
-  bitcodeContainer->GetBinaryMetadata().SetFileName(StripFileName(objectFile->getFileName()));
-  bitcodeContainer->GetBinaryMetadata().SetFileFormatName(objectFile->getFileFormatName());
-  bitcodeContainer->GetBinaryMetadata().SetArch(arch);
-  bitcodeContainer->GetBinaryMetadata().SetUuid(objectFile->getUuid().data());
+    // Create list of strings from commands separated by null bytes.
+    std::vector<std::string> cmds;
+    do {
+      // Creating a string from p consumes data until next null byte.
+      cmds.push_back(std::string(p));
+      // Continue after the null byte.
+      p += cmds.back().size() + 1;
+    } while (p < end);
 
-  return std::unique_ptr<BitcodeContainer>(bitcodeContainer);
+    return cmds;
+  }
+
+  std::string _objectPath;
+  std::string _arch;
+};
+
+BitcodeRetriever::BitcodeRetriever(std::string objectPath) : _impl(std::make_unique<Impl>(std::move(objectPath))) {}
+BitcodeRetriever::~BitcodeRetriever() = default;
+
+void BitcodeRetriever::SetArch(std::string arch) {
+  _impl->SetArch(std::move(arch));
 }
 
-std::pair<const char *, std::uint32_t> BitcodeRetriever::GetSectionData(const llvm::object::SectionRef &section) {
-  StringRef bytesStr;
-  section.getContents(bytesStr);
-  const char *sect = reinterpret_cast<const char *>(bytesStr.data());
-  uint32_t sect_size = bytesStr.size();
-  return std::make_pair(sect, sect_size);
-}
-
-std::vector<std::string> BitcodeRetriever::GetCommands(const llvm::object::SectionRef &section) {
-  auto data = GetSectionData(section);
-  const char *p = data.first;
-  const char *end = data.first + data.second;
-
-  // Create list of strings from commands separated by null bytes.
-  std::vector<std::string> cmds;
-  do {
-    // Creating a string from p consumes data until next null byte.
-    cmds.push_back(std::string(p));
-    // Continue after the null byte.
-    p += cmds.back().size() + 1;
-  } while (p < end);
-
-  return cmds;
-}
-
-bool BitcodeRetriever::processArch(std::string arch) const {
-  return _arch.empty() ? true : (_arch == arch);
+std::vector<std::unique_ptr<BitcodeContainer>> BitcodeRetriever::GetBitcodeContainers() {
+  return _impl->GetBitcodeContainers();
 }
 
 }  // namespace ebc
